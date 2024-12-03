9a778a4e80c502715e81f7be368466d8aa0a5a99
PTuano <gases-cording.0g@icloud.com>
2024-12-03 17:00:46 -0600

    Completed solutions for day 3 part 1 and part 2. TODO: Clean-up and idomatize

--- Rust/day03/src/main.rs
+++ Rust/day03/src/main.rs
@@ -58,18 +58,18 @@ impl Numbers {
 
 // Could've used string regexes here using an external create but..., 
 // ...oh well...maybe later as an enhancement or extra work.
-fn parse_input(d: &Vec<String>, p: bool) -> Result<Vec<Numbers>, String> {
+fn parse_input(d: &Vec<String>, consider: bool) -> Result<Vec<Numbers>, String> {
 
     let mut numbers_list: Vec<Numbers> = vec![];
     let long_input = d.concat();
     
     // TODO: Remove, for debugging only
-    //println!("long string:\n\n{:?}", long_input);
+    eprintln!("long string:\n\n{:?}", long_input);
 
     let mut phrase: Vec<char> = vec![];
     let mut phrase_init: bool = false;
     let mut include_phrase: Vec<char> = vec![];
-    let mut include_init: bool = true;
+    let mut include_init: bool = false;
     let mut do_include: bool = true;
 
     for c in long_input.chars() {
@@ -77,23 +77,27 @@ fn parse_input(d: &Vec<String>, p: bool) -> Result<Vec<Numbers>, String> {
             // Part 2, get the do or don't
             'd' => {
                 if !include_init {
+                    eprintln!("inc init: start push {:?}", &c);
                     include_phrase.clear();
                     include_phrase.push(c);
                     include_init = true;
                 }
+                phrase_init = false;
             },
-            'o'|'n'|'\'' => {
+            'o'|'n'|'\''|'t' => {
                 if include_init {
+                    eprintln!("inc init: push {:?}", &c);
                     include_phrase.push(c);
                 }
+                phrase_init = false;
             },
-            't' => {
+            /*'t' => {
                 if include_init && include_phrase.len() == 4 {
                     if &include_phrase[0..] == ['d', 'o', 'n', '\''] {
                         do_include = false;
                     }
                 }
-            },
+            },*/
             // we only want contiguous mul(x,y) characters
             // if there are characters in between that pattern
             // reset the phrase vector in the catch-all pattern bellow
@@ -102,70 +106,80 @@ fn parse_input(d: &Vec<String>, p: bool) -> Result<Vec<Numbers>, String> {
                     phrase.clear();
                     phrase.push(c);
                     phrase_init = true;
+                    include_init = false;
                 } else {
-                    println!("here 1: {:?}", &c);
+                    eprintln!("here 1: {:?}", &c);
                 }
             },
             'u'|'l'|'0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'|',' => {
                 if phrase_init {
                     phrase.push(c);
+                    include_init = false;
                 } else {
-                    println!("here 2: {:?}", &c);
+                    eprintln!("here 2: {:?}", &c);
                 }
             },
-            // Separate for part 2
+            // Separate '(' for part 2
             '(' => {
                 if phrase_init {
                     phrase.push(c);
+                    include_init = false;
                 }
                 if include_init {
                     include_phrase.push(c);
+                    phrase_init = false;
                 }
             },
             ')' => {
                 // TODO: A lot of assumptions here, fix them
-                if &include_phrase[0..] == ['d', 'o', '('] {
-                    do_include = true;
+                if include_init {
+                    eprintln!("!!!inc_init!!!");
+                    if &include_phrase[0..] == ['d', 'o', '('] {
+                        eprintln!("inc_init! true");
+                        do_include = true;
+                    } else if &include_phrase[0..] == ['d', 'o', 'n', '\'', 't', '('] {
+                        eprintln!("inc_init! false");
+                        do_include = false;
+                    }
                     include_phrase.clear();
                     include_init = false;
-                } else if &include_phrase[0..] == ['d', 'o', 'n', '\'', 't', '('] {
-                    do_include = false;
-                    include_phrase.clear();
-                    include_init = false;
+                    phrase_init = false;
                 }
-                if phrase_init && phrase.len() >= 7 {
-                    if &phrase[0..4] == ['m', 'u', 'l', '('] {
+                if phrase_init {
+                    include_init = false;
+                    if phrase.len() >= 7 && &phrase[0..4] == ['m', 'u', 'l', '('] {
                         let body: &[char] = &phrase[4..];
                         let body_string: String = body.iter().collect();
                         phrase.push(')');
                         let phrase_string: String = phrase.iter().collect();
                         let numbers: Vec<&str>  = body_string.split(',').collect();
                         let mut consider_number = true;
-                        if !p {
+                        if consider {
                             consider_number = do_include;
+                        } else {
+                            consider_number = true;
                         }
                         if numbers.len() == 2 {
                             let n = Numbers::new(&phrase_string, 
                                 consider_number, numbers[0], numbers[1]
                             );
                             match n {
-                                Ok(z) => numbers_list.push(z),
+                                Ok(z) => {
+                                    eprintln!("Number: {:?} | do_inc: {:?}", z, do_include);
+                                    numbers_list.push(z);
+                                },
                                 Err(e) => return Err(format!("{:?}", e))
                             };
-                        } else {
-                            println!("here 3: {:?}", &c);
                         }
-                    } else {
-                        println!("here 4: {:?}", &c);
                     }
-                } else {
-                    println!("here 5: {:?}", &c);
                 }
                 phrase_init = false;
+                include_init = false;
             },
             _ => {
-                println!("here 6: {:?}", &c);
-                phrase_init = false
+                eprintln!("here 6: {:?}", &c);
+                phrase_init = false;
+                include_init = false;
             }
         };
     }
@@ -181,7 +195,7 @@ fn puzzle_solve1(d: &Vec<String>) -> Result<u32, String> {
 
     // TODO: Remove, for debugging
     for b in batches.iter() {
-        println!(">>> b:\t{:?}", b);
+        eprintln!(">>> b:\t{:?}", b);
     }
 
     let sum_products = &batches.iter()
@@ -198,10 +212,11 @@ fn puzzle_solve2(d: &Vec<String>) -> Result<u32, String> {
 
     // TODO: Remove, for debugging
     for b in batches.iter() {
-        println!(">>> b:\t{:?}", b);
+        eprintln!(">>> b:\t{:?}", b);
     }
 
     let sum_products = &batches.iter()
+        .filter(|x|x.include)
         .fold(0, |acc, p| acc + p.product);
 
     Ok(*sum_products)
@@ -220,12 +235,9 @@ fn main() -> Result<(), String> {
     let sum_products = puzzle_solve1(&d)?;
     println!("    Sum of products: {:?}", sum_products);
 
-    /*
     println!(">>> Solving Puzzle 3 Part 2:");
-    let new_safe_reports = puzzle_solve2(&d)?;
-    let count_new_safe_reports = new_safe_reports.values().filter(|x| **x == true).count() as u32;
-    println!("\n\nNew safe reports: {:?}", count_new_safe_reports);
-    */
+    let sum_products = puzzle_solve2(&d)?;
+    println!("    Sum of products: {:?}", sum_products);
 
     Ok(())
 }
@@ -247,12 +259,12 @@ mod tests {
         Ok(())
     }
 
-    /*#[test]
+    #[test]
     fn test_puzzle_solve2() -> Result<(), String> {
-        let d= PuzzleInput::init(Some(&["this".to_string(), "test.data".to_string()]))?
+        let d= PuzzleInput::init(Some(&["this".to_string(), "test.data2".to_string()]))?
             .vectorized()?;
         let s = puzzle_solve2(&d);
-        assert_eq!(s, 48u32);
+        assert_eq!(s, Ok(48u32));
         Ok(())
-    }*/
+    }
 }
