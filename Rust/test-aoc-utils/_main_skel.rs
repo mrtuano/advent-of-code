f5e431950b7d9b13c66bc1bf0278f4ecfcd8de0b
PTuano <gases-cording.0g@icloud.com>
2024-12-05 08:45:12 -0600

    Update main.rs skeleton file to setup projects faster.

--- Rust/test-aoc-utils/_main_skel.rs
+++ Rust/test-aoc-utils/_main_skel.rs
@@ -21,16 +21,45 @@ use aoc_utils::aoc_utils::*;
    ************************************************************************* */
 
 /* *************************************************************************
-                            HELPER FUNCTIONS
+                            FUNCTIONS
    ************************************************************************* */
+fn puzzle_solve1<T>(data: &Vec<String>) -> Result<T, String> {
+    todo!();
+    Ok(results)
+}
+
+fn puzzle_solve2<T>(data: &Vec<String>) -> Result<T, String> {
+    todo!();
+    Ok(results)
+}
 
 
 /* *************************************************************************
                             MAIN PROGRAM
    ************************************************************************* */
-fn main() {
-    todo!();
+    fn main() -> Result<(), String> {
+
+    // Update as needed
+    let input_data = "input.data";
+
+    let data = PuzzleInput::init(Some(&["this".to_string(), input_data.to_string()]))?
+        .vectorized()?;
+
+    println!(">>>>>>>>>>> Puzzle Day XX <<<<<<<<<<\n");
+
+    println!("---------------");
+    println!("Solve Part 1:");
+    println!("---------------");
+    println!("  Part 1 Result: {:?}", puzzle_solve1(&data)?);
+
+    println!("---------------");
+    println!("Solve Part 2:");
+    println!("---------------");
+    println!("  Part 2 Result: {:?}", puzzle_solve2(&data)?);
+
+    Ok(())
 }
+
 
 
 /* *************************************************************************
@@ -41,7 +70,36 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_example_data() {
-        todo!();
+    fn test_puzzle_solve1() -> Result<(), String> {
+        
+        // Update as needed
+        let test_input = "test.data";
+        let test_expected = 0u32;
+
+        // Read in test data
+        let d= PuzzleInput::init(Some(&["this".to_string(), input.to_string()]))?
+            .vectorized()?;
+
+        // Test our solution
+        assert_eq!(puzzle_solve1(&d)?, Ok(test_expected));
+
+        Ok(())
+    }
+   
+    #[test]
+    fn test_puzzle_solve2() -> Result<(), String> {
+
+        // Update as needed
+        let test_input = "test.data";
+        let test_expected = 0u32;
+
+        // Read in test data
+        let d= PuzzleInput::init(Some(&["this".to_string(), input.to_string()]))?
+            .vectorized()?;
+
+        // Test our solution
+        assert_eq!(puzzle_solve2(&d)?, Ok(test_expected));
+
+        Ok(())
     }
 }
