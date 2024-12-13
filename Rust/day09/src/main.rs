77d8fcd92dcc05f29d35f3f44c163bea157e1c98
PTuano <gases-cording.0g@icloud.com>
2024-12-13 13:17:35 -0600

    Working solution to day 9 part 1.  Part 2 in progress, TBD.

--- Rust/day09/src/main.rs
+++ Rust/day09/src/main.rs
@@ -3,7 +3,7 @@
                             LIBRARIES AND DECLARATIONS
    ************************************************************************* */
 
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 
 use aoc_utils::aoc_utils::*;
 
@@ -22,139 +22,76 @@ use aoc_utils::aoc_utils::*;
                             ENUM AND METHODS
    ************************************************************************* */
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-enum ItemType {
-    Filetype,
-    Freespace
-}
 
 /* *************************************************************************
                             STRUCTURE AND METHODS
    ************************************************************************* */
 
-#[derive(Clone, Debug)]
-struct DiskItem {
-//    index: usize,
-//    raw: char,
-//    digit: u8,
-//    blocks: u8,
-    block_map: String,
-//    file_id: Option<usize>,
-//    item_type: ItemType
+#[derive(Clone, Copy, Debug)]
+struct FileItem {
+    id: Option<usize>,
+    blocks: u8,
 }
 
-impl DiskItem {
-//    fn new(index: usize, raw: &char, item_type: ItemType, 
-//        file_id: Option<usize>) -> Option<DiskItem> {
-    fn new(raw: &char, item_type: ItemType,
-        file_id: Option<usize>) -> Option<DiskItem> {
-
-        if let Ok(r) = raw_to_digit(&raw) {
-            let block_map: String = draw_item(item_type, r, file_id).unwrap_or(String::new());
-            Some(
-                DiskItem {
-//                    index,
-//                    raw: *raw,
-//                    digit: r,
-//                    blocks: r,
-//                    file_id,
-//                    item_type,
-                    block_map
-                }
-            )
-        } else {
-            None
-        }
+impl FileItem {
+    fn new(raw: &char, id: Option<usize>) -> Result<FileItem, String> {
+        let blocks = raw_to_digit(raw)?;
+        Ok(FileItem { id, blocks })
     }
 }
 
 #[derive(Debug)]
 struct DiskMap {
-//    raw: String,
-//    disk_items: Vec<DiskItem>,
-    block_map: VecDeque<char>
+    raw: String,
+    disk_items: Vec<FileItem>,
+    block_items: HashMap<usize, FileItem>
 }
 
 impl DiskMap {
 
     fn init(data: &Vec<String>) -> Result<DiskMap, String> {
 
-        //let mut disk_items: Vec<DiskItem> = vec![];
-        let mut block_items: Vec<String> = vec![];
+        let mut disk_items: Vec<FileItem> = vec![];
+        let mut block_items: HashMap<usize, FileItem> = HashMap::new();
 
-        let raw: String = data.concat();
         let mut file_id: usize = 0;
 
+        let raw: String = data.concat();
+
         for (index, char_item) in raw.chars().enumerate() {
-            let item_type: ItemType = if index % 2 == 0 {
-                ItemType::Filetype
+            if index % 2 == 0 {
+                let file_item = FileItem::new(&char_item, Some(file_id))?;
+                file_id += 1;
+                disk_items.push(file_item);
             } else {
-                ItemType::Freespace
+                let file_item = FileItem::new(&char_item, None)?;
+                disk_items.push(file_item);
             };
-            //if let Some(disk_item) = DiskItem::new(index, &char_item, item_type, Some(file_id)) {
-            if let Some(disk_item) = DiskItem::new(&char_item, item_type, Some(file_id)) {
-                block_items.push(disk_item.block_map.clone());
-                //disk_items.push(disk_item);
-                if item_type == ItemType::Filetype {
-                    file_id += 1;
-                }
-            } else {
-                return Err(format!("cannot process item {:?} as DiskItem type", char_item));
-            }
         }
 
-        let block_map: VecDeque<char> = block_items.concat().chars().into_iter().collect();
+        // TODO: Remove, for debugging only
+        //println!("Disk Map:");
+        //let _ = &disk_items.iter().for_each(|x| println!("    {:?}", x));
 
-        // TODO: Remove for debugging only.
-        //       check the number of block_map if equal to number of chars in raw input.
-        println!("Block map count: {:?}", block_map.iter().count());
-
-        Ok(
-            DiskMap {
-//                raw,
-//                disk_items,
-                block_map
-            }
-        )
-
+        Ok(DiskMap { raw, disk_items, block_items })
     }
 
-    fn remap_blocks(&self) -> VecDeque<char> {
-
-        let mut blockmap = self.block_map.clone();
-
-        let mut front_ptr = 0usize;
-        let mut back_ptr = blockmap.len() - 1;
-
-        loop {
-            if back_ptr <= front_ptr {
-                break;
-            }
-            if !need_remap(&blockmap) {
-                break;
-            }
-            let front_char = blockmap.get(front_ptr);
-            let back_char = blockmap.get(back_ptr);
-            if front_char.is_some() && back_char.is_some() {
-                if front_char.unwrap().is_ascii_digit() {
-                    front_ptr += 1;
-                    continue;
-                } 
-                if !back_char.unwrap().is_ascii_digit() {
-                    back_ptr -= 1;
-                    continue;
-                }
-                blockmap.swap(front_ptr, back_ptr);
-                front_ptr += 1;
-                back_ptr -= 1;
-            }
+    fn diskmap_to_blockmap(&self) -> VecDeque<Option<usize>> {
+        let mut block_map: VecDeque<Option<usize>> = VecDeque::new();
+        for b in self.disk_items.iter() {
+            let mut blocks: VecDeque<Option<usize>> = map_to_blocks(b);
+            block_map.append(&mut blocks);
         }
+        block_map
+    }
 
-        // TODO: Remove for debugging only
-        let xx: String = blockmap.iter().collect();
-        println!("Blockmap:\n\n{:?}", xx);
-
-        blockmap
+    fn diskmap_to_itemsmap(&self) -> VecDeque<(Option<usize>, u8)> {
+        let mut items_map: VecDeque<(Option<usize>, u8)> = VecDeque::new();
+        for b in self.disk_items.iter() {
+            let mut items: VecDeque<(Option<usize>, u8)> = map_to_items(b);
+            items_map.append(&mut items);
+        }
+        items_map
     }
 
 }
@@ -162,35 +99,145 @@ impl DiskMap {
 /* *************************************************************************
                             FUNCTIONS
    ************************************************************************* */
-fn need_remap(blocks: &VecDeque<char>) -> bool {
-    let block_str: String = blocks.iter().collect();
 
-    // TODO: Remove, for testing
-    //println!("Current: block map: {:?}", &block_str);
+/*
+fn remap_items(mut block_map: VecDeque<(usize, u8)>) -> VecDeque<Option<usize>> {
 
-    let test_vector: Vec<&str> = block_str.split('.').into_iter().collect();
-    if test_vector.len() > 1 {
-        if test_vector[1..].to_vec().iter().any(|x|x.chars().any(|y| y.is_ascii_digit())) {
-            return true;
+    let mut front_ptr = 0usize;
+    let mut back_ptr = block_map.len() - 1;
+
+    while back_ptr > front_ptr {
+        if !need_block_remap(&block_map) {
+            break;
         } else {
-            return false;
+            let front_char = block_map.get(front_ptr);
+            let back_char = block_map.get(back_ptr);
+            if front_char.is_some() && back_char.is_some() {
+                if front_char.unwrap().is_some() {
+                    front_ptr += 1;
+                    continue;
+                } 
+                if back_char.unwrap().is_none() {
+                    back_ptr -= 1;
+                    continue;
+                }
+                block_map.swap(front_ptr, back_ptr);
+                front_ptr += 1;
+                back_ptr -= 1;
+            }
+        }
+    }
+
+    let mut back_buffer = 0usize;
+    let mut prev_back: Option<usize> = None;
+    while let Some(back_char) = block_map.get(back_ptr) {
+        if back_char.is_none() {
+            back_ptr -= 1;
+            prev_back = None;
+            continue;
+        } else if prev_back == None {
+            prev_back == *back_char;
+            back_buffer += 1;
+            back_ptr -= 1;
+        } else if prev_back == *back_char {
+            back_buffer  += 1;
+            back_ptr -= 1;
+        } else if prev_back != None && prev_back != *back_char {
+            let needed = back_buffer; // amount of freespace needed to move the file
+            let start_back_ptr = back_ptr; // the pointer to the first block of the file (left to right)
+            prev_back = *back_char; // for next iteration
+            back_buffer = 0; // reset
+            back_ptr -= 1;
+            // at this point we wont move our back_ptr until we move the file
+            // or not able to find a free space.
+            if let Some(front_char) = block_map.get(front_ptr) {
+                let mut front_pointers: Vec<usize> = vec![];
+                if front_char.is_none() {
+                    front_pointers.push(front_ptr);
+                } front_char.is_some() {
+                    if !front_pointers.is_empty() && front_pointers.len() < needed {
+                        // not able to find free space, we advance to the next file
+                    }
+                }
+                    if needed == 1 {
+                        block_map.swap(front_ptr, start_back_ptr);
+                        front_ptr += 1;
+                    } else {
+                        let mut front_ptrs: Vec<usize> = vec![front_ptr];
+                    }
+                }
+            } else {
+                front_ptr +                back_ptr -= 1;
+            }
+        }
+    }
+
+    block_map
+
+}
+*/
+
+fn map_to_items(file_item: &FileItem) -> VecDeque<(Option<usize>, u8)> {
+    let mut items: VecDeque<(Option<usize>, u8)> = VecDeque::new();
+    for _ in 0..file_item.blocks {
+        items.push_back((file_item.id, file_item.blocks));
+    }
+    items
+}
+
+fn remap_blocks(mut block_map: VecDeque<Option<usize>>) -> VecDeque<Option<usize>> {
+
+    let mut front_ptr = 0usize;
+    let mut back_ptr = block_map.len() - 1;
+
+    while back_ptr > front_ptr {
+        if !need_block_remap(&block_map) {
+            break;
+        } else {
+            let front_char = block_map.get(front_ptr);
+            let back_char = block_map.get(back_ptr);
+            if front_char.is_some() && back_char.is_some() {
+                if front_char.unwrap().is_some() {
+                    front_ptr += 1;
+                    continue;
+                } 
+                if back_char.unwrap().is_none() {
+                    back_ptr -= 1;
+                    continue;
+                }
+                block_map.swap(front_ptr, back_ptr);
+                front_ptr += 1;
+                back_ptr -= 1;
+            }
+        }
+    }
+
+    block_map
+
+}
+
+fn need_block_remap(blocks: &VecDeque<Option<usize>>) -> bool {
+    let mut next_segment: bool = false;
+    while !next_segment {
+        for b in blocks.iter() {
+            if !next_segment && b.is_none() {
+                next_segment = true;
+                continue;
+            }
+            if next_segment && b.is_some() {
+                return true;
+            }
         }
     }
     false
 }
 
-fn draw_item(item_type: ItemType, blocks: u8, file_id: Option<usize>) -> Option<String> {
-    let mut draw_result: Option<String> = None;
-    if item_type == ItemType::Freespace {
-        let result: String = std::iter::repeat('.').take(blocks as usize).collect();
-        draw_result = Some(result);
-    } else {
-        if let Some(x) = file_id {
-            let result: String = std::iter::repeat(x.to_string()).take(blocks as usize).collect();
-            draw_result = Some(result);
-        }
+fn map_to_blocks(file_item: &FileItem) -> VecDeque<Option<usize>> {
+    let mut blocks: VecDeque<Option<usize>> = VecDeque::new();
+    for _ in 0..file_item.blocks {
+        blocks.push_back(file_item.id.clone());
     }
-    draw_result
+    blocks
 }
 
 fn raw_to_digit(c: &char) -> Result<u8, String> {
@@ -205,38 +252,28 @@ fn raw_to_digit(c: &char) -> Result<u8, String> {
     }
 }
 
-fn filesystem_checksum(v: VecDeque<char>) -> Result<u64, String> {
-    let mut check_sum = 0u64;
-    for (i, x ) in v.iter().enumerate() {
-        if *x == '.' {
-            break;
+fn filesystem_checksum(blocks: VecDeque<Option<usize>>) -> Result<usize, String> {
+    let mut check_sum = 0usize;
+    for (i, x) in blocks.iter().enumerate() {
+        if let Some(y) = x {
+            check_sum += i * *y;
         }
-        let num = safe_parse(&x.to_string())?;
-        check_sum += num * i as u64;
     }
     Ok(check_sum)
 }
 
-fn safe_parse(s: &str) -> Result<u64, String> {
-    if let Ok(n) = s.to_string().parse::<u64>() {
-        Ok(n)
-    } else {
-        Err(format!("cannot parse string {:?}", s))
-    }
-}
-
-fn puzzle_solve1(data: &Vec<String>) -> Result<u64, String> {
+// -------------------- PUZZLE PART 1 ------------------ //
+fn puzzle_solve1(data: &Vec<String>) -> Result<usize, String> {
     let disk_map = DiskMap::init(data)?;
-
-    // TODO: Remove, for debugging only
-    //let _ = disk_map.disk_items.iter()
-    //    .for_each(|x| println!("index: {:?}, raw: {:?}, map: {:?}", x.index, x.raw, x.block_map));
-    //println!("Block Map: {:?}", disk_map.block_map);
-
-    filesystem_checksum(disk_map.remap_blocks())
+    let remapped = remap_blocks(disk_map.diskmap_to_blockmap());
+    filesystem_checksum(remapped)
 }
 
-fn puzzle_solve2(data: &Vec<String>) -> Result<u64, String> {
+// -------------------- PUZZLE PART 2 ------------------ //
+fn puzzle_solve2(data: &Vec<String>) -> Result<usize, String> {
+    let disk_map = DiskMap::init(data)?;
+    //let remapped = remap_items(disk_map.diskmap_to_itemsmap());
+    //filesystem_checksum(remapped)
     todo!();
 }
 
@@ -281,7 +318,7 @@ mod tests {
         
         // Update as needed
         let test_input = "test.data";
-        let test_expected = 1928u64;
+        let test_expected = 1928usize;
 
         // Read in test data
         let d= PuzzleInput::init(Some(&["this".to_string(), test_input.to_string()]))?
@@ -298,7 +335,7 @@ mod tests {
 
         // Update as needed
         let test_input = "test.data";
-        let test_expected = 0u64;
+        let test_expected = 2858usize;
 
         // Read in test data
         let d= PuzzleInput::init(Some(&["this".to_string(), test_input.to_string()]))?
