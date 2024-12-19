f9ef42a2a2b3dabf5400f895ac66f296e5994a91
PTuano <gases-cording.0g@icloud.com>
2024-12-18 20:20:08 -0600

    Skeleton files for Day 11 solution

---
+++ Rust/day11/src/main.rs
@@ -0,0 +1,109 @@
+
+/* *************************************************************************
+                            LIBRARIES AND DECLARATIONS
+   ************************************************************************* */
+
+use aoc_utils::aoc_utils::*;
+
+
+/* *************************************************************************
+                            CUSTOM TYPES
+   ************************************************************************* */
+
+
+/* *************************************************************************
+                            CUSTOM TRAITS
+   ************************************************************************* */
+
+
+/* *************************************************************************
+                            ENUM AND METHODS
+   ************************************************************************* */
+
+
+/* *************************************************************************
+                            STRUCTURE AND METHODS
+   ************************************************************************* */
+
+
+/* *************************************************************************
+                            FUNCTIONS
+   ************************************************************************* */
+fn puzzle_solve1<T>(data: &Vec<String>) -> Result<T, String> {
+    todo!();
+}
+
+fn puzzle_solve2<T>(data: &Vec<String>) -> Result<T, String> {
+    todo!();
+}
+
+
+/* *************************************************************************
+                            MAIN PROGRAM
+   ************************************************************************* */
+    fn main() -> Result<(), String> {
+
+    // Update as needed
+    let input_data = "input.data";
+
+    let data = PuzzleInput::init(Some(&["this".to_string(), input_data.to_string()]))?
+        .vectorized()?;
+
+    println!("\n>>>>>>>>>>> Puzzle Day XX <<<<<<<<<<\n");
+
+    println!("---------------");
+    println!("Solve Part 1:");
+    println!("---------------\n");
+    println!("  Part 1 Result: {:?}\n\n", puzzle_solve1(&data)?);
+
+    println!("---------------");
+    println!("Solve Part 2:");
+    println!("---------------\n");
+    println!("  Part 2 Result: {:?}\n\n", puzzle_solve2(&data)?);
+
+    Ok(())
+}
+
+
+
+/* *************************************************************************
+                            TESTING
+   ************************************************************************* */
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_puzzle_solve1() -> Result<(), String> {
+        
+        // Update as needed
+        let test_input = "test.data";
+        let test_expected = 0u32;
+
+        // Read in test data
+        let d= PuzzleInput::init(Some(&["this".to_string(), test_input.to_string()]))?
+            .vectorized()?;
+
+        // Test our solution
+        assert_eq!(puzzle_solve1(&d)?, test_expected);
+
+        Ok(())
+    }
+   
+    #[test]
+    fn test_puzzle_solve2() -> Result<(), String> {
+
+        // Update as needed
+        let test_input = "test.data";
+        let test_expected = 0u32;
+
+        // Read in test data
+        let d= PuzzleInput::init(Some(&["this".to_string(), test_input.to_string()]))?
+            .vectorized()?;
+
+        // Test our solution
+        assert_eq!(puzzle_solve2(&d)?, test_expected);
+
+        Ok(())
+    }
+}
